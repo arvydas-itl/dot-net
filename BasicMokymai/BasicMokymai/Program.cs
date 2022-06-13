73ab338521f776a1cbbc4a9489e167fb98e57873
arvydas-itl <itl.arvydas@gmail.com>
2022-06-13 16:03:12 +0300

    Enviroment newline paaiskinimas

--- BasicMokymai/BasicMokymai/Program.cs
+++ BasicMokymai/BasicMokymai/Program.cs
@@ -5,12 +5,16 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
-            Console.ReadKey();
+            
 
             Console.WriteLine("------------------------------------");
             Console.WriteLine("išvedimas" + "vienoje " + "eilutėje"); // konkatinacija
             Console.WriteLine("{0} {1} {2}", "isvedimas", "vienoje", "eiluteje"); // kompozicija
             Console.WriteLine($"{"isvedimas"} {"vienoje"} {"eiluteje"}"); // interpoliacija
+
+            Console.WriteLine("------------------------------------");
+            Console.WriteLine($"tekstas {Environment.NewLine} idedamas"); // perkelia teksta i nauja eilute nepriklausomai nuo OS
+            Console.ReadKey();
         }
     }
 }
