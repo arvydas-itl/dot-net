3a08fe1552edc1065ae51f1a5fcd400b0180767e
arvydas-itl <itl.arvydas@gmail.com>
2022-06-13 15:40:12 +0300

    string konkatinacija, kompozicija, interpoliacija pavyzdziai

--- BasicMokymai/BasicMokymai/Program.cs
+++ BasicMokymai/BasicMokymai/Program.cs
@@ -5,6 +5,12 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+            Console.ReadKey();
+
+            Console.WriteLine("------------------------------------");
+            Console.WriteLine("išvedimas" + "vienoje " + "eilutėje"); // konkatinacija
+            Console.WriteLine("{0} {1} {2}", "isvedimas", "vienoje", "eiluteje"); // kompozicija
+            Console.WriteLine($"{"isvedimas"} {"vienoje"} {"eiluteje"}"); // interpoliacija
         }
     }
 }
